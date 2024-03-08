c0bd63f458fd009b7bfa5507c0488aed7bf022a5
DevTruce <DevTruce@Gmail.com>
2024-03-07 20:27:13 -0500

    added reveal on scroll

--- src/script.js
+++ src/script.js
@@ -107,6 +107,7 @@ const stickyNav = function (entries) {
   if (!entry.isIntersecting) nav.classList.add("sticky");
   else nav.classList.remove("sticky");
 };
+
 const headerObserver = new IntersectionObserver(stickyNav, {
   root: null,
   threshold: 0,
@@ -114,3 +115,24 @@ const headerObserver = new IntersectionObserver(stickyNav, {
 });
 
 headerObserver.observe(header);
+
+////////////////////////////////////////////////////////////////
+//// Reveal Sections
+const allSections = document.querySelectorAll(".section");
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove("section--hidden");
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add("section--hidden");
+});
